ec793397138e311bb6a943cc4eae6c65bafe4cff
Sam Hodgkinson <sam.hodgkinson@avanade.com>
2022-10-19 18:02:22 +0100

    task4 complete

---
+++ boolean.py
@@ -0,0 +1,12 @@
+from operator import truediv
+from telnetlib import theNULL
+
+
+num1 = float(input('Enter your first number: '))
+num2 = float(input('Enter your second number: '))
+if num1 > num2:
+    num1big = True
+else:
+    num1big = False
+
+print('It is', num1big, 'that the first number is bigger')
