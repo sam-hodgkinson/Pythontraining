a2fdadc8f7482382c5724776baf5f9042d84896e
Sam Hodgkinson <sam.hodgkinson@avanade.com>
2022-10-20 10:40:02 +0100

    finished taks

---
+++ temperaturegauge.py
@@ -0,0 +1,9 @@
+temperature = 30
+if (temperature >= 30):
+    print("too hot" "\n" "aghh")
+elif (temperature >= 50):
+    print("ahhhh")
+elif (temperature < 0):
+    print("too cold")
+else:
+    print("just right")
